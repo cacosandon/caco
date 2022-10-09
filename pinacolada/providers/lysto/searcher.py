8cac200f7e82016d18bfa75be14c24ae942a92bd
cacosandon <jiossandon@uc.cl>
2022-10-09 13:42:01 -0300

    feat(lysto@searcher): return empty list if commune not supported

--- pinacolada/providers/lysto/searcher.py
+++ pinacolada/providers/lysto/searcher.py
@@ -1,6 +1,7 @@
 from pinacolada.base_searcher import BaseSearcher
 from pinacolada.providers.lysto.scraper import LystoScraper
-from pinacolada.utils import deep_get
+from pinacolada.utils import deep_get, get_location_data
+from pinacolada.providers.lysto.constants import AVAILABLE_COMMUNES
 
 class LystoSearcher(BaseSearcher):
   """
@@ -13,13 +14,26 @@ class LystoSearcher(BaseSearcher):
     self.scraper = LystoScraper()
 
   def get_products(self, search_term: str):
+    if not self.location_accepted_by_provider:
+      return []
+
     products_data = self.scraper.get_products_data(search_term = search_term)
 
     return list(map(self.__parse_product_data, products_data))
 
   def __parse_product_data(self, product_data: dict[str, str]):
     return {
-      'name': product_data['name'],
-      'image_url': product_data['image'],
+      'name': deep_get(product_data, 'name'),
+      'image_url': deep_get(product_data, 'image'),
       'price_amount': float(deep_get(product_data, 'offers', 'price'))
     }
+
+  def location_accepted_by_provider(self):
+     location_data = get_location_data(self.latitude, self.longitude)
+
+     return any(
+        [
+          deep_get(location_data, 'city') in AVAILABLE_COMMUNES,
+          deep_get(location_data, 'suburb') in AVAILABLE_COMMUNES
+        ]
+      )
