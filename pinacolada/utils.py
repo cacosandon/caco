615d80ef9fcd3b877d0a47ba86f702fe3686d072
cacosandon <jiossandon@uc.cl>
2022-10-09 13:21:27 -0300

    feat(utils): add location data from lat and long

--- pinacolada/utils.py
+++ pinacolada/utils.py
@@ -1,5 +1,9 @@
+import requests
+from xmljson import parker
+from xml.etree.ElementTree import fromstring
 from functools import reduce
 from typing import List
+
 
 def deep_get(dictionary: dict(), *keys: List[str]):
     """
@@ -7,3 +11,10 @@ def deep_get(dictionary: dict(), *keys: List[str]):
     """
 
     return reduce(lambda hash, key: hash.get(key) if hash else None, keys, dictionary)
+
+def get_location_data(latitude: str, longitude: str) -> dict[str, str]:
+    data_url = f"https://nominatim.openstreetmap.org/reverse?lat={latitude}&lon={longitude}"
+
+    location_xml_data = fromstring(requests.get(data_url).text)
+
+    return parker.data(location_xml_data)['addressparts']
